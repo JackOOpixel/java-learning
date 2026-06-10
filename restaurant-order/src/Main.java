5d984a5c493821858fedfe0f5d39005ceff3db9f
Mickael <micks.gestion+GIT@gmail.com>
2026-06-10 15:59:27 +0200

    Création du sous projet restaurant-order

---
+++ restaurant-order/src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("===============================");
+        System.out.println("APPLICATION DE PRISE DE COMMANDE");
+        System.out.println("===============================");
+
+        System.out.println("");
+        System.out.println("Restaurant: Chez Mickaël");
+        System.out.println("Serveur connecté: Mickaël");
+        System.out.println("Table: 12");
+        System.out.println("");
+        System.out.println("Commande N°1");
+        System.out.println("");
+        System.out.println("Burger x2");
+        System.out.println("Frites x1");
+        System.out.println("Coca x3");
+
+    }
+}
