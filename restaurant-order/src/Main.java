fbcd27a5ccb30497c6ab8fd648e103f6faaef09e
Mickael <micks.gestion+GIT@gmail.com>
2026-06-10 16:02:54 +0200

    Complétion 1er jour: challenge fin de journée

--- restaurant-order/src/Main.java
+++ restaurant-order/src/Main.java
@@ -11,9 +11,10 @@ public class Main {
         System.out.println("");
         System.out.println("Commande N°1");
         System.out.println("");
-        System.out.println("Burger x2");
-        System.out.println("Frites x1");
-        System.out.println("Coca x3");
+        System.out.println("Burger x2 : 24 $");
+        System.out.println("Frites x1 : 4 $");
+        System.out.println("Coca x3 : 9 $");
 
+        System.out.println("TOTAL : 37 $");
     }
 }
